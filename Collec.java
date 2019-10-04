5ffc34ab0620471031304faeebc00a0236be17b5
YKlijia <56055065+YKlijia@users.noreply.github.com>
2019-10-04 11:23:13 +0800

    Add files via upload

---
+++ Collec.java
@@ -0,0 +1,20 @@
+﻿
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Collec {
+    public static void main(String[] args) {
+        ArrayList<String> c = new ArrayList<>();
+        c.add("刘备");
+        c.add("关羽");
+        c.add("张飞");
+        c.add("诸葛亮");
+        c.add("赵云");
+        Iterator<String> it = c.iterator();
+        while(it.hasNext()){
+            String s = it.next();
+            System.out.println(s);
+        }
+    }
+}
