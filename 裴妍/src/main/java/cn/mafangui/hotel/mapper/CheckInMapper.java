70470518a9b89ffc42766347da87df55aa97064d
peiyan531 <56054975+peiyan531@users.noreply.github.com>
2019-10-30 13:16:45 +0800

    Add files via upload

---
+++ 裴妍/src/main/java/cn/mafangui/hotel/mapper/CheckInMapper.java
@@ -0,0 +1,30 @@
+package cn.mafangui.hotel.mapper;
+
+import cn.mafangui.hotel.entity.CheckIn;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public interface CheckInMapper {
+    int deleteByPrimaryKey(Integer checkInId);
+
+    int insert(CheckIn record);
+
+    int insertSelective(CheckIn record);
+
+    CheckIn selectByPrimaryKey(Integer checkInId);
+
+    CheckIn selectLatestByRoomNumber(String roomNumber);
+
+    int updateByRoomNumber(String roomNumber);
+
+    int checkOut(Integer checkInId);
+
+    int updateByPrimaryKeySelective(CheckIn record);
+
+    int updateByPrimaryKey(CheckIn record);
+
+    List<CheckIn> selectAll();
+
+}
